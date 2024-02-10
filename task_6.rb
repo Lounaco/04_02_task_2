40fe49d136d863720fb3cc207eb84a10ddcdc7b5
kristinashinkarenco <squirrelonair@gmail.com>
2024-02-10 22:53:16 +0300

    10_02/4

---
+++ task_6.rb
@@ -0,0 +1,10 @@
+#Taking input
+puts "Enter the product name: "
+name_of_product = gets.chomp.to_i
+
+puts "Enter the price per unit: "
+unit_price = gets.chomp.to_i
+
+puts " Enter amonth of purchased products: "
+amount_of_product= gets.chomp.to_f
+

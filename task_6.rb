18234ecf4b260070b165a2cb54cd83bd335124f7
kristinashinkarenco <squirrelonair@gmail.com>
2024-02-11 08:49:32 +0300

    11_02/1

--- task_6.rb
+++ task_6.rb
@@ -1,6 +1,18 @@
+#Initialize the hash
+
+shopping_list= {
+	name_of_product: { unit_price, amount_of_product }
+}
+
 #Taking input
+
+puts " Hi! Let's count your expenses? 
+When you finish entering your shopping list, 
+enter 'stop' in the product name. "
+
 puts "Enter the product name: "
-name_of_product = gets.chomp.to_i
+name_of_product = gets.chomp
+name_of_product.capitalize!
 
 puts "Enter the price per unit: "
 unit_price = gets.chomp.to_i
@@ -8,3 +20,7 @@ unit_price = gets.chomp.to_i
 puts " Enter amonth of purchased products: "
 amount_of_product= gets.chomp.to_f
 
+
+when 
+	name_of_product = 'stop'
+	  puts "Total purchase prise is #{}"
